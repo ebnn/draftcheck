1f5d2edb2c0473b1ca0f1d02a079c58ad54879ce
partialsum@hotmail.com <partialsum@hotmail.com>
2014-08-12 21:30:29 +1000

    Add setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='draftcheck',
+      version='0.1',
+      description='LaTeX Lint for Academic Writing',
+      packages=['draftcheck'],
+)
